084a8c2a82d230ab8d41e822349df2a6b27a5e51
Lueezyy <luis.balb@icloud.com>
2026-09-25 18:29:16 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, gateway!");
+
+Console.Write("Sender name: ");
+string? sender = Console.ReadLine();
+
+Console.Write("Recipient name: ");
+string? recipient = Console.ReadLine();
+
+Console.Write("Currency: ");
+string? currency = Console.ReadLine();
+
+Console.WriteLine($"Sender is {sender}");
+Console.WriteLine($"Recipient is {recipient}");
+Console.WriteLine($"Currency is {currency}");
